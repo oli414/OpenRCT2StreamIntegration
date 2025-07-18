6066586e1ec433f952c16f49e036bd014f4ff4d5
Tux2 <thetux2@gmail.com>
2025-07-17 20:02:53 -0600

    Refined random redeems. Added ability to use words for weather.

--- src/actions/Action.js
+++ src/actions/Action.js
@@ -6,10 +6,26 @@ class Action {
     }
 
     trigger(params) {
+        var newMessage = params.message;
+        if(this.identifier === "FORCE_WEATHER") {
+            if(params.message.toLowerCase().includes("sun")) {
+                newMessage = "0";
+            }else if(params.message.toLowerCase().includes("partly")) {
+                newMessage = "1";
+            }else if(params.message.toLowerCase().includes("cloud")) {
+                newMessage = "2";
+            }else if(params.message.toLowerCase().includes("heavy")) {
+                newMessage = "4";
+            }else if(params.message.toLowerCase().includes("rain")) {
+                newMessage = "3";
+            }else if(params.message.toLowerCase().includes("storm") || params.message.toLowerCase().includes("thunder")) {
+                newMessage = "5";
+            }
+        }
         this.actionManager.send({
             type: this.identifier,
             username: params.username,
-            message: params.message
+            message: newMessage
         });
     }
 }
--- src/actions/RandomAction.js
+++ src/actions/RandomAction.js
@@ -17,14 +17,16 @@ const good_things = [
     "FIX_VANDALISM",
     "REMOVE_LITTER",
     "FIX_RIDES",
-    "EMPTY_BLADDERS"
+    "EMPTY_BLADDERS",
+    "FORCE_WEATHER"
 ];
 
 const neutral_things = [
     "GIVE_PEEPS_PARK_MAPS",
     "GIVE_PEEPS_BALLOONS",
     "GIVE_PEEPS_UMBRELLAS",
-    "SPAWN_DUCKS"
+    "SPAWN_DUCKS",
+    "FORCE_WEATHER"
 ];
 
 const all_things = [
@@ -50,7 +52,7 @@ const all_things = [
 ];
 
 function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
+  return Math.floor(Math.random() * (max + 0.9));
 }
 
 class RandomAction extends Action {
@@ -60,10 +62,29 @@ class RandomAction extends Action {
     }
 
     trigger(params) {
+        var newMessage = params.message;
+        var idType = (this.identifier === "RANDOM_BAD_THING")?bad_things[getRandomInt(bad_things.length)]:((this.identifier === "RANDOM_GOOD_THING")?good_things[getRandomInt(good_things.length)]:all_things[getRandomInt(all_things.length)]);
+        if(idType === "FORCE_WEATHER") {
+            switch (this.identifier) {
+                case "RANDOM_BAD_THING":
+                    newMessage = "5";
+                    break;
+                case "RANDOM_GOOD_THING":
+                    newMessage = "0";
+                    break;
+                case "RANDOM_THING":
+                    newMessage = getRandomInt(6) + "";
+                    break;
+            
+                default:
+                    newMessage = getRandomInt(6) + "";
+                    break;
+            }
+        }
         this.actionManager.send({
-            type: (this.identifier === "RANDOM_BAD_THING")?bad_things[getRandomInt(bad_things.length)]:((this.identifier === "RANDOM_GOOD_THING")?good_things[getRandomInt(good_things.length)]:all_things[getRandomInt(all_things.length)]),
+            type: idType,
             username: params.username,
-            message: params.message
+            message: newMessage
         });
     }
 }
